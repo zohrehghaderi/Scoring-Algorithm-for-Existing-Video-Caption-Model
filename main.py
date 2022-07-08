6ad1314396cac7d9af9af390d58fb67b0a001203
misheel <misheel.ganbold@student.uni-tuebingen.de>
2022-07-08 14:03:23 +0200

    changed text output

--- main.py
+++ main.py
@@ -3,16 +3,15 @@ import numpy as np
 import tempfile
 import streamlit as st
 import torch
-import cv2
 from Swin_BERT_Semantics import Swin_BERT_Semantics
 
 
-in_video = st.file_uploader('Dataloader Test', type=['mp4'])
+in_video = st.file_uploader('Video Caption Test', type=['mp4'])
 if in_video is not None:
+    st.video(in_video)
     tfile = tempfile.NamedTemporaryFile(delete=False)
     tfile.write(in_video.read())
     mydataloader = Dataloader.Video_Caption_Loader(config="swin_base_bert.py")
-    #images = mydataloader.__getitem__(tfile.name)
     device = 'cpu'
     model = Swin_BERT_Semantics(mlp_freeze=False, swin_freeze=True, in_size=1024, hidden_sizes=[2048, 1024],
                                 out_size=768, drop_bert=0, max_length=20)
@@ -29,5 +28,6 @@ if in_video is not None:
     generate_text = output.cpu().numpy().tolist()
 
     generate_converted = model.tokenizer.batch_decode(generate_text, skip_special_tokens=True)
-
-    st.write(generate_converted)
+    st.write('The generated Caption is:')
+    st.info(generate_converted[0])
+    st.write('')
